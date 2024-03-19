d1ab18d6e946eb2d21478aae8815e09a0652fa8d
iamvanessag <vangoncalvesilva@gmail.com>
2024-03-19 07:35:13 -0300

    Criando Primeiro Modelo

---
+++ src/main/java/com/exemplospringboot/todosimple/models/User.java
@@ -0,0 +1,29 @@
+package com.exemplospringboot.todosimple.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name=User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull
+    @NotEmpty
+    private String password;
+}
